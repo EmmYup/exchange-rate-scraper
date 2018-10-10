84023d8a873a597548f3c777b44df16c586c5ae3
Emmanuel Yupit <emmanuelyupit08@gmail.com>
2018-10-10 10:11:08 -0500

    now getting mexicos currenvy

--- api/services/ScraperService.js
+++ api/services/ScraperService.js
@@ -2,14 +2,14 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 const getCurrencies = async () => {
-  const url = `http://www.bna.com.ar/Personas#divisas`;
-  const { data: html } = await axios(url);
-
-  const $ = cheerio.load(html);
+  const urlArgentina = `http://www.bna.com.ar/Personas#divisas`;
+  let { data: html } = await axios(urlArgentina);
+  let $ = cheerio.load(html);
   let currencies = [];
   let argentina = [];
   $('tr', '#divisas').each((i, elm) => {
     argentina.push({
+      country: 'argentina',
       currency: $(elm)
         .children()
         .first()
@@ -32,6 +32,38 @@ const getCurrencies = async () => {
     argentina[0]
   );
   currencies.push(argentina);
+  const urlMexico = 'https://bbv.infosel.com/bancomerindicators/indexV7.aspx';
+  let { data: htmlmx } = await axios(urlMexico);
+  $ = cheerio.load(htmlmx);
+  let mexico = [];
+  let values = [];
+  $('.tbl-info-financiera tr').each((i, elm) => {
+    var x = $(elm)
+      .children('td')
+      .text()
+      .trim();
+    if (x.indexOf('Dólar Compra') > -1) {
+      values[0] = $(elm)
+        .children('td')
+        .next()
+        .text()
+        .trim();
+    }
+    if (x.indexOf('Dólar Venta') > -1) {
+      values[1] = $(elm)
+        .children('td')
+        .next()
+        .text()
+        .trim();
+    }
+  });
+  mexico.push({
+    country: 'mexico',
+    currency: 'Dolar U.S.A',
+    buy: values[0],
+    sell: values[1],
+  });
+  currencies.push(mexico[0]);
   return currencies;
 };
 
